19341da38f68a86bd86a713b51907c608bf4c951
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-06-24 22:21:16 +0100

    Extract streaming
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- 03-install-root-fs/root-fs.sh
+++ 03-install-root-fs/root-fs.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+echo "Starting deployment"
+
 source functions.sh && init
 set -o nounset
 
 BASEURL="http://$MIRROR_HOST/misc/osie/current"
-assetdir=/tmp/assets
-mkdir $assetdir
-wget "$BASEURL/image.img.tgz" -P $assetdir
-cd $assetdir
+# assetdir=/tmp/assets
+# mkdir $assetdir
 
-tar -xvf image.img.tgz
-dd if=./image.img of=/dev/sda
+curl -s "$BASEURL/image.img.tgz" --output - |tar -xzvf - > /dev/sda
+
+# wget "$BASEURL/image.img.tgz" -P $assetdir
+# cd $assetdir
+
+# tar -xvf image.img.tgz
+# dd if=./image.img of=/dev/sda
