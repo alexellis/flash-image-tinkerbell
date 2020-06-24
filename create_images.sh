b12039b40b6f1d224006449d373f56adbfe8f39d
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-06-24 21:49:58 +0100

    Update latest version
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- create_images.sh
+++ create_images.sh
@@ -6,6 +6,6 @@ docker push 192.168.1.1/ubuntu:base
 docker build -t 192.168.1.1/disk-wipe:v1 01-disk-wipe/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/disk-wipe:v1
 
-docker build -t 192.168.1.1/install-root-fs:v1 03-install-root-fs/ --build-arg REGISTRY=192.168.1.1
-docker push 192.168.1.1/install-root-fs:v1
+docker build -t 192.168.1.1/install-root-fs:v2 03-install-root-fs/ --build-arg REGISTRY=192.168.1.1
+docker push 192.168.1.1/install-root-fs:v2
 
